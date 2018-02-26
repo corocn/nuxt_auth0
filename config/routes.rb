53f215f5024c21b0ede4306eaf54463a107071f9
corocn <crsk.plmzh@gmail.com>
2018-02-27 00:36:07 +0900

    ping apiの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :ping
+    end
+  end
 end
